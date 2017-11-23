46ed447ce87749c9b208c9304df30bf38a645e73
Sam <sam.rr.lee@gmail.com>
2017-11-23 13:38:55 +0000

    Fixed positioning of software stripe

--- js/main.js
+++ js/main.js
@@ -1,18 +1,5 @@
-onloads.push(function() {
-  var softwares = document.getElementsByClassName("software");
-  var white_software, black_software;
-  for (var i = 0; i < softwares.length; i++) {
-    if (softwares[i].classList.contains("is-white")) {
-      white_software = softwares[i];
-    } else if (softwares[i].classList.contains("is-black")) {
-      black_software = softwares[i];
-    }
-  }
-  white_software.style["margin-top"] = "-" + black_software.getClientRects()[0].height + "px";
-});
-
 window.onload = function() {
   for (var i = 0; i < onloads.length; i++) {
-    onloads[i]()
+    onloads[i]();
   }
 }
